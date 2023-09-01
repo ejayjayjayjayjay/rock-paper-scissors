c314b2f7d4bd08ef12d4ce526f1cf532fb640f09
Josell Vibar <vibarjosell@gmail.com>
2023-09-01 20:44:55 +0800

    add simple ui to front end and add logic to the game computer selection and player selection

--- index.js
+++ index.js
@@ -1,70 +1,93 @@
-let playerScore = 0;
-let computerScore = 0;
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
+// Get the element
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsBtn = document.querySelector('#scissors');
+let playerScore = document.querySelector('#player-score');
+let computerScore = document.querySelector('#computer-score');
+let resultTie = document.querySelector('#result-tie');
+
+// Global variables to keep track of the round results
+let computerWinAmount = 0;
+let playerWinAmount = 0;
+let tieAmount = 0;
+
+// function of event
+
+function playGame(e) {
+    let playerSelection = e.target.id;
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+}
+
+rockBtn.addEventListener("click", playGame);
+paperBtn.addEventListener("click", playGame);
+scissorsBtn.addEventListener("click", playGame);
+
+// computer choose random rock,paper,scissors
 
 function getComputerChoice () {
     let choices = ["rock","paper", "scissors"];
     return randomChoice = choices[Math.floor(Math.random() * choices.length)]; 
 }
 
-function getPlayerChoice() {
-    let playerChoices = prompt("Choose if 'rock' (1), 'paper' (2), 'scissors' (3)");
-    
-    if (playerChoices.toLowerCase() === "rock" || parseInt(playerChoices) === 1) {
-         playerChoices === "rock";
-    } else if (playerChoices.toLowerCase() === "paper" || parseInt(playerChoices) === 2) {
-         playerChoices === "paper";
-    } else if (playerChoices.toLowerCase() === "scissors" || parseInt(playerChoices) === 3 ) {
-         playerChoices === "scissors";
-    } else if (playerChoices.toLowerCase() !== "rock" || "paper" || "scissors" || parseInt(playerChoices) !== 1 || 2 || 3) {
-        alert("You must choose! dumbass!");
-        playerChoices;
-    } else {
-        console.log("can't decide?");
-    }
-     return playerChoices;
+// Group of functions to change text labels after rounds
+function playerWin() {
+    ++playerWinAmount;
+    playerScore.innerHTML = "Your score is: " + playerWinAmount;
 }
 
-function playRound(playerSelection,computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "Tie game!!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++;
-        return "You lose! Computer win! Paper beats rock";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore++;
-        return "You lose! Computer win! Scissors beats paper";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++;
-        return "You lose! Computer win! Rock beats scissors";
-    } else {
-        playerScore++;
-        return "Player win !!"
-    }
+function computerWin() {
+    ++computerWinAmount;
+    computerScore.innerHTML = "The computer score is: " + computerWinAmount;
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        let currentRound = playRound(playerSelection,computerSelection);
-
-        console.log(currentRound);
-        console.log("Player Score: " + playerScore + " vs " + "Computer Score: " + computerScore);
-    }
+function tieWin() {
+    ++tieAmount;
+    resultTie.innerHTML = "Tie rounds: " + tieAmount;
 }
 
-function winnerResults() {
-    if (playerScore === computerScore) {
-        console.log("We have a tie breaker");
-    } else if (playerScore > computerScore) {
-        console.log("Overall winner: Player!!");
-    } else {
-        console.log("Overall winner: Computer!!");
+// game function
+
+// Function that checks winning very inelegantly
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" &&
+    computerSelection === "rock") {
+        tieWin();
+    } else if (playerSelection === "rock" &&
+    computerSelection === "scissors") {
+        playerWin();
+    } else if (playerSelection === "rock" &&
+    computerSelection === "paper") {
+        computerWin();
+    }
+
+    if (playerSelection === "paper" &&
+    computerSelection === "rock") {
+        playerWin();
+    } else if (playerSelection === "paper" &&
+    computerSelection === "paper") {
+        tieWin();
+    } else if (playerSelection === "paper" &&
+    computerSelection === "scissors") {
+        computerWin();
+    }
+
+    if(playerSelection === "scissors" &&
+    computerSelection === "rock") {
+        computerWin();
+    } else if (playerSelection === "scissors" &&
+    computerSelection === "paper") {
+        playerWin();
+    } else if (playerSelection === "scissors" &&
+    computerSelection === "scissors") {
+        tieWin();
+    }
+
+    if(computerWinAmount === 5) {
+        alert("You lost! Better luck next time :(");
+        reset();
+    } else if (playerWinAmount === 5) {
+        alert("You won! Congratulations! :)")
+        reset();
     }
 }
-
-game();
-winnerResults();
-
